db6ba07792aed3180ba6b7f52da8ca6faf0710ad
Erik Sundin <mail@eriksundin.com>
2024-08-02 14:55:37 +0200

    added neovim config

--- flake.nix
+++ flake.nix
@@ -3,17 +3,18 @@
 
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    neovim.url = "github:erik-sundin-git/neovim";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs,neovim, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = {
+	  inherit inputs;
+	};
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
+        ./config.nix
       ];
     };
   };
