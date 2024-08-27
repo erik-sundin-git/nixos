1ea384c3d7ad854a215cbcae9097c9cea878920c
Erik Sundin <mail@eriksundin.com>
2024-08-28 01:43:10 +0200

    converted config.nix working on nixos modules

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,9 +1,8 @@
 {
   imports = [
-    ./services.nix
     #    ./samba-client.nix
-    ./xdg-portal.nix
-    ./davfs2.nix
+#    ./xdg-portal.nix
+#    ./davfs2.nix
     ../../stylix/default.nix
     #    ./ssh-share.nix
   ];
--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -10,16 +10,5 @@
   services.xserver.windowManager.qtile.enable = true;
   #services.desktopManager.plasma6.enable = true;
   services.davfs2.enable = true;
-
   services.flatpak.enable = true;
-
-  #############
-  #   Steam   #
-  #############
-
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession = {
-    enable = true;
-    args = ["-h 720" "-H 1080" "-F fsr"];
-  };
 }
