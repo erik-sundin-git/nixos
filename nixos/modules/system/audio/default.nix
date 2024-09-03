494e10ca812477242261fcfda1f998f3aa29eb9f
Erik Sundin <mail@eriksundin.com>
2024-09-03 19:31:46 +0200

    Initial test

---
+++ nixos/modules/system/audio/default.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+let
+  cfg = config.system.audio;
+in
+{
+  options = {
+
+    system.audio.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable system audio";
+    };
+
+    system.audio.desktop = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable some packages only for desktop";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    hardware.pulseaudio.enable = false;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    environment.systemPackages = mkIf cfg.desktop [
+
+    ];
+  };
+}
