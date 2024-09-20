e640a42ec29d54e3a8fb1e705653825f2aa3e7c7
Erik Sundin <erik@thinkpad.eriksundin.com>
2024-09-20 18:12:19 +0200

    Debian host: Packages

--- nixos/hosts/debian/home.nix
+++ nixos/hosts/debian/home.nix
@@ -1,10 +1,12 @@
 {
   config,
   pkgs,
+  pkgs-unstable,
   inputs,
   ...
-}:
-{
+}: let
+
+in {
   home.username = "erik";
   home.homeDirectory = "/home/erik";
 
@@ -14,31 +16,33 @@
     #inputs.qtile-flake.packages.x86_64-linux.qtile
     pkgs.qtile
     inputs.neovim.defaultPackage.x86_64-linux
-    pkgs.coreutils-full
     pkgs.wlroots
     pkgs.ncspot
-    pkgs.woeusb
+    pkgs.sct
+    pkgs.tor-browser-bundle-bin
+    pkgs-unstable.picom
     pkgs.weylus
-  #  pkgs.spotify
+    pkgs-unstable.spotify
     pkgs.quickemu
     pkgs.nixfmt-rfc-style
     pkgs.firefox
+
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    (pkgs.writeShellScriptBin "set-brightness" "$HOME/dotfiles/scripts/set-brightness.sh")
   ];
-  home.file."${config.home.homeDirectory}/.config/qtile/" = {
-    source = "${inputs.dots}/.config/qtile/";
-    recursive = true;
-  };
 
   home.sessionVariables = {
     EDITOR = "emacs";
   };
 
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = ["nix-command" "flakes"];
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  #rofi.enable = true;
 }
