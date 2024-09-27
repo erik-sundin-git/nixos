855cb7643dd8e55f294629ee903a260b43b69a05
Erik Sundin <erik@thinkpad.eriksundin.com>
2024-09-27 23:20:28 +0200

    flake update

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -1,5 +1,7 @@
-{dots, ...}: {
+{ dots, ... }:
+{
   imports = [
     ./rofi
+    ./polybar
   ];
 }
---
+++ home-manager/modules/polybar/default.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  lib,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
+with lib; {
+  options = {
+    polybar.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable or disable the polybar-module.";
+    };
+  };
+
+  config = mkIf config.polybar.enable {
+    services.polybar.enable = true;
+
+    environment.systemPackages = [
+    ];
+  };
+}
